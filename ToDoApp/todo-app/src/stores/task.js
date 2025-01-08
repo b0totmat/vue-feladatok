867c58da344dfaa1ab77b3b23d9571b4b9d2b4f2
Toth-Mate <t0thm4t3@gmail.com>
2025-01-08 10:16:34 +0100

    TaskStore

---
+++ ToDoApp/todo-app/src/stores/task.js
@@ -0,0 +1,38 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('task', () => {
+  const tasks = ref([
+    {
+      id: 0,
+      title: 'Bevásárlás',
+      desc: 'Kell venni tejet, jégkockát, plasztik evőeszközöket a jövőheti ereszdelahajamhoz.',
+      isFinished: false,
+      deadline: '2024-01-10'
+    },
+    {
+      id: 1,
+      title: 'Állatorvos',
+      desc: 'A kutyának egy hete megy a hasa, el kellene vinni állatorvoshoz.',
+      isFinished: true,
+      deadline: '2024-01-25'
+    },
+    {
+      id: 2,
+      title: 'Takarítás',
+      desc: 'Hogy néz ki ez a lakás, te?! Vendégek mit fognak szólni?',
+      isFinished: false,
+      deadline: '2024-01-09'
+    }
+  ])
+  function deleteTask(id) {
+    const taskToDeleteIdx = tasks.value.findIndex(t => t.id === id)
+    tasks.value.splice(taskToDeleteIdx, 1)
+  }
+  function finishTask(id) {
+    const taskIdx = tasks.value.find(t => t.id === id)
+    tasks[taskIdx].isFinished = true
+  }
+
+  return { tasks, deleteTask, finishTask }
+})
