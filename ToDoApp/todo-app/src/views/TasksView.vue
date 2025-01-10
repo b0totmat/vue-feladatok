4e13dc9f0cac74789fe7c0a366807f6fa871cbc1
Toth-Mate <t0thm4t3@gmail.com>
2025-01-10 09:28:28 +0100

    Reszponzív elrendezés

--- ToDoApp/todo-app/src/views/TasksView.vue
+++ ToDoApp/todo-app/src/views/TasksView.vue
@@ -1,21 +1,17 @@
 <template>
     <div class="container">
-        <ul class="list-group">
-            <li v-for="task in tasks.tasks" class="list-group-item">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ task.title }}</h5>
-                        <p class="card-text">{{ task.desc }}</p>
-                        <p class="card-text">Állapot: {{ task.isFinished ? "Kész" : "Nincs kész" }}</p>
-                        <p class="card-text text-danger">Határidő: {{ task.deadline }}</p>
-                        <a class="btn btn-primary" v-if="!task.isFinished" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-check"></i></a>
-                        <a class="btn btn-warning" v-if="task.isFinished" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-x"></i></a>
-                        <a class="btn btn-danger mx-1" @click="tasks.deleteTask(task.id)"><i class="fa-solid fa-trash"></i></a>
-                        <a class="btn btn-secondary" v-if="!task.isFinished" @click="redirect(task.id)"><i class="fa-solid fa-pen-to-square"></i></a>
-                    </div>
-                </div>
-            </li>
-        </ul>
+        <div class="tasks row">
+            <div class="task col col-12 col-lg-3 col-md-6 border p-2 me-2 mb-2" v-for="task in tasks.tasks">
+                <h5>{{ task.title }}</h5>
+                <p class="text">{{ task.desc }}</p>
+                <p class="text">Állapot: {{ task.isFinished ? "Kész" : "Nincs kész" }}</p>
+                <p class="text text-danger">Határidő: {{ task.deadline }}</p>
+                <a class="btn btn-primary" v-if="!task.isFinished" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-check"></i></a>
+                <a class="btn btn-warning" v-if="task.isFinished" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-x"></i></a>
+                <a class="btn btn-danger mx-1" @click="tasks.deleteTask(task.id)"><i class="fa-solid fa-trash"></i></a>
+                <a class="btn btn-secondary" v-if="!task.isFinished" @click="redirect(task.id)"><i class="fa-solid fa-pen-to-square"></i></a>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
