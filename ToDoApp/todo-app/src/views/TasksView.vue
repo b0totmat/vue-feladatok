2cd1f77276e4d70f2406d27e923cded6502bdbec
Toth-Mate <t0thm4t3@gmail.com>
2025-01-10 08:54:45 +0100

    A határidőt is megjelenítjük a feladatok listájában

--- ToDoApp/todo-app/src/views/TasksView.vue
+++ ToDoApp/todo-app/src/views/TasksView.vue
@@ -7,6 +7,7 @@
                         <h5 class="card-title">{{ task.title }}</h5>
                         <p class="card-text">{{ task.desc }}</p>
                         <p class="card-text">Állapot: {{ task.isFinished ? "Kész" : "Nincs kész" }}</p>
+                        <p class="card-text text-danger">Határidő: {{ task.deadline }}</p>
                         <a class="btn btn-primary" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-check"></i></a>
                         <a class="btn btn-danger mx-1" @click="tasks.deleteTask(task.id)"><i class="fa-solid fa-trash"></i></a>
                         <a class="btn btn-secondary" @click="redirect(task.id)"><i class="fa-solid fa-pen-to-square"></i></a>
