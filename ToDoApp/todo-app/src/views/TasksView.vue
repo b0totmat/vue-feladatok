b5719522dc243fbfce483efc1bfd3db5ffdf6a7b
Toth-Mate <t0thm4t3@gmail.com>
2025-01-10 09:13:31 +0100

    Módosító gomb csak akkor, ha nincs kész a feladat

--- ToDoApp/todo-app/src/views/TasksView.vue
+++ ToDoApp/todo-app/src/views/TasksView.vue
@@ -8,9 +8,10 @@
                         <p class="card-text">{{ task.desc }}</p>
                         <p class="card-text">Állapot: {{ task.isFinished ? "Kész" : "Nincs kész" }}</p>
                         <p class="card-text text-danger">Határidő: {{ task.deadline }}</p>
-                        <a class="btn btn-primary" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-check"></i></a>
+                        <a class="btn btn-primary" v-if="!task.isFinished" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-check"></i></a>
+                        <a class="btn btn-warning" v-if="task.isFinished" @click="tasks.finishTask(task.id)"><i class="fa-solid fa-x"></i></a>
                         <a class="btn btn-danger mx-1" @click="tasks.deleteTask(task.id)"><i class="fa-solid fa-trash"></i></a>
-                        <a class="btn btn-secondary" @click="redirect(task.id)"><i class="fa-solid fa-pen-to-square"></i></a>
+                        <a class="btn btn-secondary" v-if="!task.isFinished" @click="redirect(task.id)"><i class="fa-solid fa-pen-to-square"></i></a>
                     </div>
                 </div>
             </li>
